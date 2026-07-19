6ff34282570b7e55f70215ccc0c97221657ef0eb
Samridhi Shukla <samriddhishukla77@gmail.com>
2026-07-19 22:51:25 +0530

    Add files via upload

---
+++ Tuple.py
@@ -0,0 +1,13 @@
+# Tuple is immutable
+tup = (89,78,9)
+print(tup)
+print(type(tup))
+ # single value is ended with a comma otherwise python takes it as a integer or other data type
+u =(90) 
+print(type(u))
+i = (67,)
+print(type(i))
+
+t = ("C","D","A","A","A","B","B","A")
+print(t.count("A"))
+
